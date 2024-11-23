14189515ced8b54cf00c04d6e0c21da95b1a297d
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-11-23 08:08:31 -0300

    adicionando funcionalidade

--- banco.py
+++ banco.py
@@ -1,5 +1,6 @@
 import conta
 import cliente
+import random
 
 
 class Banco:
@@ -10,4 +11,60 @@ class Banco:
         self.cliente = cliente or []
 
 
+    def checa_agencia(self, agencia):
+        return agencia in self.agencias or False
     
+
+    def checa_conta(self, conta):
+        return conta in self.contas or False
+    
+    def novo_cliente_corrente(self, nome, idade, saldo):
+        """
+        Cria um novo cliente com conta corrente
+
+        num_conta devereceber um inteiro aleatorio de 7 digitos para ser o numero da conta
+
+        agencia recebe um inteiro de 4 digito como numero da agencia
+        
+        cc é a instância da ContaCorrente usando as variaveis previamente definidas como argumento
+
+        cliente_conta define na classe cliente qual é a conta dele
+
+        em seguida tudo é adicionado nas variaveis de classe
+        """
+        num_conta = random.randint(1,9999999)
+        agencia = random.randint(1, 9999)
+        cc = conta.ContaCorrente(agencia, num_conta, saldo)
+        cliente_conta = cliente.Cliente(nome, idade)
+        cliente_conta.conta = cc
+        self.contas.append(cc)
+        self.agencias.append(agencia)
+        self.cliente.append(cliente_conta)
+        print(f'Conta: {num_conta}',
+              f'Agência: {agencia}')
+        
+
+    def novo_cliente_poupanca(self,nome, idade, saldo):
+        """
+        Cria um novo cliente com conta poupanca
+
+        num_conta devereceber um inteiro aleatorio de 7 digitos para ser o numero da conta
+
+        agencia recebe um inteiro de 4 digito como numero da agencia
+        
+        cc é a instância da ContaCorrente usando as variaveis previamente definidas como argumento
+
+        cliente_conta define na classe cliente qual é a conta dele
+
+        em seguida tudo é adicionado nas variaveis de classe
+        """
+        num_conta = random.randint(1,9999999)
+        agencia = random.randint(1, 9999)
+        cc = conta.ContaPoupanca(agencia, num_conta, saldo)
+        cliente_conta = cliente.Cliente(nome, idade)
+        cliente_conta.conta = cc
+        self.contas.append(cc)
+        self.agencias.append(agencia)
+        self.cliente.append(cliente_conta)
+        print(f'Conta: {num_conta}',
+              f'Agência: {agencia}')
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import banco, conta, cliente
 
+Santander = banco.Banco()
+
+Santander.novo_cliente_corrente("José", 203, 1456295)
+
+print()
